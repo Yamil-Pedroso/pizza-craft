100dab4237c871c1d4a81e8417bbd97b4b251ea8
Yami Pedroso-AI <yamirovinci@gmail.com>
2024-04-25 09:19:57 +0200

    Adjust margin global

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -1,51 +1,51 @@
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-import { IPizza } from "../types/types";
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
+import { IPizza } from '../types/types'
 
 const containerVariants = {
   hidden: {
     opacity: 0,
-    x: "100vw",
+    x: '100vw',
   },
   visible: {
     opacity: 1,
     x: 0,
     transition: {
       delay: 0.5,
-      type: "spring",
+      type: 'spring',
     },
   },
   exit: {
-    x: "-100vw",
-    transition: { ease: "easeInOut" },
+    x: '-100vw',
+    transition: { ease: 'easeInOut' },
   },
-};
+}
 
 const nextVariants = {
   hidden: {
-    x: "100vw",
+    x: '100vw',
   },
   visible: {
     x: 0,
-    transition: { type: "spring", stiffness: 120 },
+    transition: { type: 'spring', stiffness: 120 },
   },
-};
+}
 
 const buttonVariants = {
   hover: {
     scale: 1.1,
-    textShadow: "0px 0px 8px rgb(255,255,255)",
-    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
     transition: {
       yoyo: Infinity,
       duration: 0.3,
     },
   },
-};
+}
 
 const Base = ({ addBase, pizza }: IPizza) => {
-  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
-  
+  const bases = ['Classic', 'Thin & Crispy', 'Thick Crust']
+
   return (
     <div>
       <motion.div
@@ -62,13 +62,12 @@ const Base = ({ addBase, pizza }: IPizza) => {
             <p>{thinCrispy}</p>
             <p>{thickCrust}</p>
           </div>
-        </div>*/
-        }
+        </div>*/}
         <div>
           <h3>Step 1: Choose Your Base</h3>
           <ul>
             {bases.map((base) => {
-              const spanClass = pizza.base === base ? "active" : "";
+              const spanClass = pizza.base === base ? 'active' : ''
 
               return (
                 <motion.li
@@ -77,16 +76,16 @@ const Base = ({ addBase, pizza }: IPizza) => {
                   whileHover={{
                     scale: 1.1,
                     originX: 0,
-                    color: "#f8e112",
+                    color: '#f8e112',
                   }}
                   transition={{
-                    type: "spring",
+                    type: 'spring',
                     stiffness: 300,
                   }}
                 >
                   <span className={spanClass}>{base}</span>
                 </motion.li>
-              );
+              )
             })}
           </ul>
 
@@ -101,13 +100,15 @@ const Base = ({ addBase, pizza }: IPizza) => {
           )}
         </div>
         <div>
-          <img 
-           style={{ width: 200, borderRadius: 5 }}
-          src="https://plus.unsplash.com/premium_photo-1673439299792-d5b80707851b?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="pizza" />
+          <img
+            style={{ width: 250, borderRadius: 5 }}
+            src="https://plus.unsplash.com/premium_photo-1673439299792-d5b80707851b?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="pizza"
+          />
         </div>
       </motion.div>
     </div>
-  );
-};
+  )
+}
 
-export default Base;
+export default Base
--- src/components/Toppings.tsx
+++ src/components/Toppings.tsx
@@ -1,56 +1,56 @@
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-import images from "../assets/images";
-import { IoCheckmark } from "react-icons/io5";
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
+import images from '../assets/images'
+import { IoCheckmark } from 'react-icons/io5'
 
 const containerVariants = {
   hidden: {
     opacity: 0,
-    x: "100vw",
+    x: '100vw',
   },
   visible: {
     opacity: 1,
     x: 0,
     transition: {
       delay: 0.5,
-      type: "spring",
+      type: 'spring',
     },
   },
   exit: {
-    x: "-100vw",
-    transition: { ease: "easeInOut" },
+    x: '-100vw',
+    transition: { ease: 'easeInOut' },
   },
-};
+}
 
 const buttonVariants = {
   hover: {
     scale: 1.1,
-    textShadow: "0px 0px 8px rgb(255,255,255)",
-    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
     transition: {
       yoyo: Infinity,
       duration: 0.3,
     },
   },
-};
+}
 
 interface ITopings {
-  addTopping: (topping: string) => void;
+  addTopping: (topping: string) => void
   pizza: {
-    base: string;
-    toppings: string[];
-  };
+    base: string
+    toppings: string[]
+  }
 }
 
 const Toppings = ({ addTopping, pizza }: ITopings) => {
   const toppings = [
-    "mushrooms",
-    "peppers",
-    "onions",
-    "olives",
-    "extra cheese",
-    "tomatoes",
-  ];
+    'mushrooms',
+    'peppers',
+    'onions',
+    'olives',
+    'extra cheese',
+    'tomatoes',
+  ]
   const toppingIcons = [
     images.mushrooms,
     images.chiliPeppers,
@@ -58,7 +58,7 @@ const Toppings = ({ addTopping, pizza }: ITopings) => {
     images.olives,
     images.extraCheese,
     images.tomatoes,
-  ];
+  ]
 
   return (
     <>
@@ -69,21 +69,18 @@ const Toppings = ({ addTopping, pizza }: ITopings) => {
         animate="visible"
         exit="exit"
       >
-        <img src={images.pizzaChorrea} alt="pizza" className="pizza" 
+        <img
+          src={images.pizzaChorrea}
+          alt="pizza"
+          className="pizza"
           style={{ width: 400 }}
         />
-        <div
-          style={{
-            
-          }}
-        >
+        <div style={{}}>
           <h3>Step 2: Choose Toppings</h3>
           <ul>
             {toppings.map((topping) => {
-              const isActive = pizza.toppings.includes(topping);
-              const spanClass = pizza.toppings.includes(topping)
-                ? "active"
-                : "";
+              const isActive = pizza.toppings.includes(topping)
+              const spanClass = pizza.toppings.includes(topping) ? 'active' : ''
 
               return (
                 <div className="topping-wrapper">
@@ -95,20 +92,18 @@ const Toppings = ({ addTopping, pizza }: ITopings) => {
                       originX: 0,
                     }}
                     transition={{
-                      type: "spring",
+                      type: 'spring',
                       stiffness: 300,
                     }}
                   >
                     <span className={spanClass}>{topping}</span>
                   </motion.li>
-                  <div
-                   key={topping}
-                  >
+                  <div key={topping}>
                     {isActive ? (
                       <div
                         style={{
-                          position: "relative",
-                          display: "inline-block",
+                          position: 'relative',
+                          display: 'inline-block',
                           width: 50,
                         }}
                       >
@@ -119,9 +114,9 @@ const Toppings = ({ addTopping, pizza }: ITopings) => {
                         />
                         <IoCheckmark
                           style={{
-                            position: "absolute",
-                            left: "3.5rem",
-                            color: "green",
+                            position: 'absolute',
+                            left: '3.5rem',
+                            color: 'green',
                             fontSize: 30,
                           }}
                         />
@@ -130,24 +125,23 @@ const Toppings = ({ addTopping, pizza }: ITopings) => {
                       <img
                         src={toppingIcons[toppings.indexOf(topping)]}
                         alt={topping}
-                        style={{ width: 50, filter: "grayscale(100%)" }}
+                        style={{ width: 50, filter: 'grayscale(100%)' }}
                       />
                     )}
                   </div>
                 </div>
-              );
+              )
             })}
           </ul>
+          <Link to="/order">
+            <motion.button variants={buttonVariants} whileHover="hover">
+              Order
+            </motion.button>
+          </Link>
         </div>
-
-        <Link to="/order">
-          <motion.button variants={buttonVariants} whileHover="hover">
-            Order
-          </motion.button>
-        </Link>
       </motion.div>
     </>
-  );
-};
+  )
+}
 
-export default Toppings;
+export default Toppings
